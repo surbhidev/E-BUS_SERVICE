54a2c30ddae393713856f2c69daf8fc67120cea6
Surbhi <129314114+AryaSur@users.noreply.github.com>
2023-07-03 00:56:59 +0530

    Delete SDF PROJECT directory

--- SDF PROJECT/responsive.js
+++
@@ -1,9 +0,0 @@
-function showSection(sectionId) {
-    const sections = document.getElementsByClassName("section-content");
-    for (let i = 0; i < sections.length; i++) {
-      sections[i].style.display = "none";
-    }
-    document.getElementById(sectionId).style.display = "block";
-  }
-  
-  
